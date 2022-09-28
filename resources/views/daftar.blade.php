b8ebd252295eb0748c4a08c1a7b1fee70ac6ddbd
BabuTugas <gamingindonesia0768@gmail.com>
2022-09-28 14:19:09 +0700

    make form input

--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -18,7 +18,54 @@
   </div>
 <br>
 <form action="" method="POST">
-
+      <div class="form-group">
+        <label class="form-control-label" for="exampleFormControlInput1">No Pasien</label>
+        <input name="nopasien" type="number" class="form-control" id="exampleFormControlInput1" placeholder="No Pasien">
+        @error('no') {{ $massage }} @enderror
+      </div>
+      <div class="form-group">
+        <label class="form-control-label" for="exampleFormControlInput1">Nama Pasien</label>
+        <input name="nama" type="name" class="form-control" id="exampleFormControlInput1" placeholder="Nama Pasien">
+        @error('nama') {{ $massage }} @enderror
+      </div>
+      <div class="form-group">
+        <label class="form-control-label" for="exampleFormControlInput1">NIK</label>
+        <input name="nik" type="number" class="form-control" id="exampleFormControlInput1" placeholder="NIK">
+        @error('nik') {{ $massage }} @enderror
+      </div>
+      <div class="form-group">
+        <label class="form-control-label" for="exampleFormControlInput1">Tanggal Lahir</label>
+        <input name="tgllhr" type="date" class="form-control" id="exampleFormControlInput1" placeholder="Tanggal Lahir">
+      </div>
+      <div class="form-group">
+        <label class="form-control-label" for="exampleFormControlInput1">Tempat Lahir</label>
+        <input name="tmplhr" type="text" class="form-control" id="exampleFormControlInput1" placeholder="Tempat Lahir">
+      </div>
+      <div class="form-group">
+        <label class="form-control-label" for="exampleFormControlInput1">Umur</label>
+        <input name="umur" type="number" class="form-control" id="exampleFormControlInput1" placeholder="Umur">
+      </div>
+      <div class="form-group">
+        <label class="form-control-label" for="exampleFormControlInput1">Alamat</label>
+        <input name="alamat" type="text" class="form-control" id="exampleFormControlInput1" placeholder="Alamat">
+      </div>
+      <div class="form-group">
+        <label class="form-control-label" for="exampleFormControlInput1">No Telpon</label>
+        <input name="nohp" type="number" class="form-control" id="exampleFormControlInput1" placeholder="No Telpon">
+        @error('nohp') {{ $massage }} @enderror
+      </div>
+      <div class="form-group">
+        <label class="form-control-label" for="exampleFormControlSelect1">Jenis Kelamin</label>
+        <select name="jk" class="form-control" id="exampleFormControlSelect1">
+          <option class="disabled">Jenis Kelamin</option>
+          <option value="laki-laki">Laki-Laki</option>
+          <option value="perempuan">Perempuan</option>
+        </select>
+      </div>
+      <div class="col-lg-6 col-5 text-right">
+          <button type="submit" class="btn btn-sm btn-neutral btn-secondary">Kirim</button>
+      </div>
+<br>
 </form>
 
 @endsection
