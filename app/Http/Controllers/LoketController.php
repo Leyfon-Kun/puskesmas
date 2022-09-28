16ffc297e17c1ae1562d9f10772e8ba3e2126517
BabuTugas <gamingindonesia0768@gmail.com>
2022-09-28 16:57:34 +0700

    fix create pasien

--- app/Http/Controllers/LoketController.php
+++ app/Http/Controllers/LoketController.php
@@ -28,22 +28,37 @@ class LoketController extends Controller
     public function save(Request $request)
     {
         $request->validate([
-            'no' => 'required',
+            'nopasien' => 'required',
             'nama' => 'required',
             'nik' => 'required',
-            'nohp' => 'required'
+            'tgllhr' => 'required',
+            'tmplhr' => 'required',
+            'umur' => 'required',
+            'alamat' => 'required',
+            'nohp' => 'required',
+            'jk' => 'required'
         ],[
-            'no.required' => 'diisi ya sayangkuh',
-            'nama.required' => 'diisi ya sayangkuh',
-            'nik.required' => 'diisi ya sayangkuh',
-            'nohp.required' => 'diisi ya sayangkuh'
+            'nopasien.required' => 'Harap Diisi',
+            'nama.required' => 'Harap Diisi',
+            'nik.required' => 'Harap Diisi',
+            'tgllhr.required' => 'Harap Diisi',
+            'tmplhr.required' => 'Harap Diisi',
+            'umur.required' => 'Harap Diisi',
+            'alamat.required' => 'Harap Diisi',
+            'nohp.required' => 'Harap Diisi',
+            'jk.required' => 'Harap diisi'
         ]);
 
         $result = [
-            'no' => Request()->no,
-            'nama' => request()->nama,
-            'nik' => request()->nik,
-            'nohp' => request()->no
+            'no_pasien' => Request()->nopasien,
+            'nama_pasien' => Request()->nama,
+            'nik' => Request()->nik,
+            'tgl_lahir' => Request()->tgllhr,
+            'tmpt_lahir' => Request()->tmplhr,
+            'umur' => Request()->umur,
+            'alamat' => Request()->alamat,
+            'telpon' => Request()->nohp,
+            'jk' => Request()->jk
         ];
 
         $this->Loket->createData($result);
