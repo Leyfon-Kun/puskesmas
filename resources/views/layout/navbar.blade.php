ca26cdf3d0bebf1a70e544613acd9cb7d672814f
Leyfon-Kun <uzumakibimbim073@gmail.com>
2022-09-28 22:47:40 +0700

    fix username view

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -140,7 +140,7 @@
               <span aria-hidden="true">×</span>
             </button>
           </form>
-          <ul class="navbar-nav align-items-center ml-auto ml-md-10">
+          <ul class="navbar-nav align-items-center ml-auto">
             <li class="nav-item dropdown">
               <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <div class="media align-items-center">
@@ -148,7 +148,7 @@
                     <img alt="Image placeholder" src="{{ asset('template') }}/assets/img/theme/team-4.jpg">
                   </span>
                   <div class="media-body ml-2 d-none d-lg-block">
-                    <span class="mb-0 text-sm  font-weight-bold">Admin-Kun</span>
+                    <span class="mb-0 text-sm  font-weight-bold">{{ auth()->user()->username }}</span>
                   </div>
                 </div>
               </a>
