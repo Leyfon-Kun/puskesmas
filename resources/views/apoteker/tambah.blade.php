4e0f18a9adc436f42ba9c6cf109c934fb8a96a84
BabuTugas <gamingindonesia0768@gmail.com>
2022-11-06 18:58:36 -0800

    new

---
+++ resources/views/apoteker/tambah.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.navbar')
+
+@section('content')
+    <br>
+    <br>
+    <br>
+    <div class="header">
+        <div class="container-fluid">
+            <div class="header-body">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 col-7">
+                        <h2 class="h2 text-black d-inline-block mb-0">Daftar Pasien</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
